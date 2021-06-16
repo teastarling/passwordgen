dcd78836a154fd47e354e592ae3b4ec69c23d8e1
Tyler Starling <tyler.a.starling@gmail.com>
2021-06-16 17:01:55 -0400

    created functions to generate random numbers, letters(upper and lower case), and symbols; event on click created (not final, only testing)

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# passwordgen
-password generator created with javascript
+# PasswordGen
+*Password generator that, on click, prompts user for letter case, numbers, symbols, and length of password before generating a password based on user parameters
+
+##Changes
+
+*Created functions to generate random values within an array
+
+*Put together function to generate alert on 'click'
+
+
---
+++ script.js
@@ -0,0 +1,79 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var upperCaseSel = window.prompt("Include uppercase letters?");
+//   var lowerCaseSel = window.prompt("Include lowercase letters?");
+//   var numberSel = window.prompt("Include numbers?");
+//   var symbolSel = window.prompt("Include symbols?");
+//   var lengthSel = window.prompt("Please state desired password length.");
+
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Generator functions
+// var upperCaseSel = window.prompt("Include uppercase letters?");
+// var lowerCaseSel = window.prompt("Include lowercase letters?");
+// var numberSel = window.prompt("Include numbers?");
+// var symbolSel = window.prompt("Include symbols?");
+// var lengthSel = window.prompt("Please state desired password length.")
+
+
+// var generateSel =
+// var displaySel =
+
+
+
+
+
+
+// var randomFunc = {
+//   upper: randomUpper,
+//   lower: randomLower,
+//   number: randomNumber,
+//   symbol: randomSymbol
+// };
+
+// // function to generate a random upper-case letter
+// function randomUpper() {
+//   var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+//   return upperCase[Math.floor(Math.random() * upperCase.length)];
+// }
+
+// // function to generate a random lower-case letter
+// function randomLower() {
+//   var lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+//   return lowerCase[Math.floor(Math.random() * lowerCase.length)];
+// }
+
+// // function to generate a random number
+// function randomNumber() {
+//   var numbers = '0123456789'
+//   return numbers[Math.floor(Math.random() * numbers.length)];
+// }
+
+// // function to generate a random symbol
+// function randomSymbol() {
+//   var symbols = '!#$%&()*+,-./:;<=>?@[]^{|}~';
+//   return symbols[Math.floor(Math.random() * symbols.length)];
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword()) {
+// };
+
+// console.log(randomLower());
+// console.log(randomUpper());
+// console.log(randomNumber());
+// console.log(randomSymbol());
+function masAtBtn() {
+  alert("WORKING");
+}
+
+generateBtn.addEventListener("click", masAtBtn)
+
